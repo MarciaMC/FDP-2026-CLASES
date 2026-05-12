135ab4768c7331821c23351cdf543a1336398f68
Marcia Valentina Méndez Cabrera <00143326@uca.edu.sv>
2026-05-12 17:39:29 -0600

    ejercicios de while

---
+++ while/cuentaregresiva.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int num;
+    cout<<"escriba un numero"<<endl;
+    cin>>num;
+    while( num >= 0) {
+    cout<< num <<endl;
+    num--; }
+    return 0;
+}
---
+++ while/menuinteractivo.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int opcion = 0;
+    while(opcion != 3)
+    {
+        cout<<"menu"<<endl;
+        cout<<"1. saludar"<<endl;
+        cout<<"2. mensaje"<<endl;
+        cout<<"3. salir"<<endl;
+        cin>>opcion;
+        if(opcion ==1){
+            cout<<"Hola!"<<endl; }
+            else if(opcion == 2){
+                cout<<"como estas?"<<endl; }
+                else if(opcion ==3){
+                    cout<<"saliendo del programa"<<endl;
+                } else { cout<<"opcion no valida"<<endl;}
+
+
+            }
+    
+    
+
+    return 0;}
