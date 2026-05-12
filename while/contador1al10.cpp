60e12a858acd1d8760860094fef513bf4740626a
Marcia Valentina Méndez Cabrera <00143326@uca.edu.sv>
2026-05-11 20:25:12 -0600

    contador de 1 al 10

---
+++ while/contador1al10.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int contador = 1;
+    while(contador <= 10) {
+        cout<< contador << endl;
+        contador++;
+
+    }
+    return 0;
+}
