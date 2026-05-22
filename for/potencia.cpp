1399dac806052daf73e386bcf0c5d437a8718f5e
Marcia Valentina Méndez Cabrera <00143326@uca.edu.sv>
2026-05-21 19:16:35 -0600

    nuevo ejercicio for

---
+++ for/potencia.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int base, exponente, resultado = 1;
+    cout << "digite su numero base" << endl;
+    cin >> base;
+    cout << "digite su numero exponente" << endl;
+    cin >> exponente;
+    for (int i = 0; i < exponente; i++)
+    {
+        resultado = resultado * base;
+    } cout << resultado;
+    return 0;
+}
